54a73825671b8408408fca6388c23aae655e24c7
muneunji <ansdmswl0722@gmail.com>
2023-05-31 10:42:37 +0900

    ConfigurationProperties

--- src/main/java/com/nhnacademy/springbootstudent/SpringBootStudentApplication.java
+++ src/main/java/com/nhnacademy/springbootstudent/SpringBootStudentApplication.java
@@ -1,18 +1,16 @@
 package com.nhnacademy.springbootstudent;
 
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+//@ConfigurationPropertiesScan
 @SpringBootApplication
 public class SpringBootStudentApplication {
 
 	public static void main(String[] args) {
 //		SpringApplication.run(SpringBootStudentApplication.class, args);
-		SpringApplication application = new SpringApplication((SpringBootStudentApplication.class));
+		SpringApplication application = new SpringApplication(SpringBootStudentApplication.class);
 
 		application.run(args);
 	}
---
+++ src/main/java/com/nhnacademy/springbootstudent/configuration/StudentAutoConfiguration.java
@@ -0,0 +1,10 @@
+package com.nhnacademy.springbootstudent.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(StudentProperties.class)
+public class StudentAutoConfiguration {
+}
